793556a2ad2a8c8fd42203a173c9d60b0fe73cf9
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-19 19:42:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,15 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+<<<<<<< HEAD
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+=======
+  def happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+>>>>>>> bed8512925be3112f2bbd2328e05aa353c41bfd0
   end
 end
 
